90204ce9282e50bb72c8ac37df946959dc421120
Daniel Fowler <me@danfowler.net>
2015-12-16 14:54:53 +0300

    Add mapping, datapackage creation

--- migrate/export.py
+++ migrate/export.py
@@ -1,7 +1,11 @@
-import os
-import json
+import csv
+import datapackage
 import dataset
+import io
+import json
+import os
 from datetime import datetime
+from jsontableschema import infer
 from normality import slugify
 
 DB_URI = os.environ.get('DB_URI')
@@ -13,6 +17,68 @@ def json_default(obj):
     if isinstance(obj, datetime):
         return obj.isoformat()
 
+def create_datapackage(ds):
+    # Create datapackage based on dataset.json
+    dp = datapackage.DataPackage()
+    basepath = 'exports/{}'.format(ds['name'])
+    dp.metadata['name'] = ds['name']
+    dp.metadata['title'] = ds['label']
+    dp.metadata['description'] = ds['description']
+    dp.metadata['countryCode'] = ds['territories']
+    dp.metadata['license'] =  "ODbL-1.0"
+    dp.metadata['profiles'] = {'fiscal': '*','tabular': '*'}
+    dp.metadata['resources'] = [{}]
+    resource = dp.resources[0]
+    resource.metadata['name'] = 'dataset'
+    resource.metadata['path'] = 'dataset.csv'
+    
+    # Infer schema of dataset.csv file
+    with io.open(basepath + '/dataset.csv') as stream:
+        headers = stream.readline().rstrip('\n').split(',')
+        values = csv.reader(stream)
+        schema = infer(headers, values)
+        resource.metadata['schema'] = schema
+
+    # Translate mapping
+    dp.metadata['mapping'] = fdp_mapping(ds)
+
+    # Write datapackage.json
+    with open(basepath + '/datapackage.json', 'w') as f:
+        f.write(dp.to_json())
+
+def fdp_mapping(ds):
+    ds_mapping = ds.get('data').get('mapping')
+    fdp_mapping = {
+        'measures': {},
+        'dimensions': {}
+    }
+    keys_to_remove = ['default_value','dimension','type', 'datatype', 'column', 'description', 'label', 'facet', 'key']
+
+    for element in ds_mapping:
+        if ds_mapping[element].get('type') == 'measure':
+            fdp_mapping['measures'][element] = ds_mapping[element]
+            fdp_mapping['measures'][element]['source'] = element
+            fdp_mapping['measures'][element]['currency'] = ds['currency']
+        else:
+            # TODO: handle time as special case
+            if element == 'time':
+                continue
+            fdp_mapping['dimensions'][element] = ds_mapping[element]
+            for attribute in fdp_mapping['dimensions'][element]['attributes']:
+                fdp_mapping['dimensions'][element]['attributes'][attribute]['source'] = ('_').join([element,attribute])
+                for k in keys_to_remove:
+                    if k in fdp_mapping['dimensions'][element]['attributes'][attribute]:
+                        fdp_mapping['dimensions'][element]['attributes'][attribute].pop(k)
+            fdp_mapping['dimensions'][element]['primaryKey'] = 'none'
+
+    for md in ['measures','dimensions']:
+        for element in fdp_mapping[md]:
+            for k in keys_to_remove:
+                if k in fdp_mapping[md][element]:
+                    fdp_mapping[md][element].pop(k)
+                    
+    return fdp_mapping
+        
 def get_mappings():
     for ds in list(engine['dataset']):
         if ds['name'] not in DATASETS_TO_EXPORT:
@@ -44,8 +110,7 @@ def get_mappings():
             continue
 
         yield ds, mapping
-
-
+    
 def get_queries():
     for ds, mapping in get_mappings():
         ds_name = ds['name']
@@ -98,10 +163,11 @@ def freeze_all():
             ds_path = os.path.join(path, 'dataset.json')
             with open(ds_path, 'w') as fh:
                 json.dump(ds, fh, default=json_default, indent=2)
-
             res = engine.query(query)
             dataset.freeze(res, filename='dataset.csv', prefix=path,
                            format='csv')
+            create_datapackage(ds)
+
         except Exception as e:
             print(e)
 
